44b6e4b21f307c4304b1a7a7b6d801869657c12e
songyanbin <598349146>
2024-07-01 14:13:55 +0800

    页面的优化

---
+++ utils/dictData.js
@@ -0,0 +1,125 @@
+const overTimeOptions = [
+  {
+    label: '双休',
+    value: '1'
+  },
+  {
+    label: '弹性工作',
+    value: '2'
+  },
+  {
+    label: '偶尔加班',
+    value: '3'
+  }
+]
+const scaleList = [{
+  label: '0-20人',
+  value: '0-20人'
+}, {
+  label: '20-99人',
+  value: '20-99人'
+}, {
+  label: '100-499人',
+  value: '100-499人'
+}]
+const isOrYes = [{
+  label: '是',
+  value: '1'
+}, {
+  label: '否',
+  value: '0'
+}]
+const financingOptions = [{
+  label: '未融资',
+  value: '1'
+}, {
+  label: '不需要融资',
+  value: '8'
+},
+{
+  label: '天使轮',
+  value: '1'
+}, {
+  label: 'A轮',
+  value: '2'
+}, {
+  label: 'B轮',
+  value: '3'
+}, {
+  label: 'C轮',
+  value: '4'
+}, {
+  label: 'D轮',
+  value: '5'
+}, {
+  label: 'E轮',
+  value: '6'
+}, {
+  label: 'IPO',
+  value: '7'
+}]
+// 
+const welfareOptions = [{
+  label: '交通补助',
+  value: '1'
+}, {
+  label: '生日福利',
+  value: '2'
+}, {
+  label: '节日福利',
+  value: '3'
+}, {
+  label: '生日福利',
+  value: '4'
+}, {
+  label: '团建聚餐',
+  value: '5'
+}, {
+  label: '零食下午茶',
+  value: '6'
+}, {
+  label: '餐补',
+  value: '7'
+}, {
+  label: '员工旅游',
+  value: '8'
+}, {
+  label: '带薪年假',
+  value: '9'
+}, {
+  label: '加班补助',
+  value: '10'
+}, {
+  label: '企业年金',
+  value: '11'
+}, {
+  label: '年终奖',
+  value: '12'
+}, {
+  label: '定期体检',
+  value: '13'
+}]
+const industryOptions = [{
+  label: '互联网',
+  value: '1'
+}, {
+  label: '企业服务',
+  value: '2'
+}, {
+  label: '通信/网络设备',
+  value: '3'
+}, {
+  label: '计算机软件',
+  value: '4'
+}, {
+  label: '电子商务',
+  value: '5'
+}]
+export default {
+  overTimeOptions,
+  scaleList,
+  financingOptions,
+  welfareOptions,
+  industryOptions,
+  isOrYes
+}
